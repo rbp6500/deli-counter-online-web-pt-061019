a623753642ffd5845be4983bb854330567045770
Rashard Pleasant <rashard.pleasant@gmail.com>
2019-06-19 22:05:15 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,18 @@
-# Write your code here.
+require
+Katz_deli = []
+ => ["The line is currently empty"]
+ puts ("someone should be in the line")
+
+end
+
+take_a_number = ("name")
+ => ("Katz_deli"["name"])
+ puts ("Katz_deli ["Mike"])
+ 
+ end
+
+ Now_serving = ("number")
+  puts ("There is nobody waiting to be served")
+   => ("Now serving ["mike"])
+ 
+ end
