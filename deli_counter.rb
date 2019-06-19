c93ef2811e1a48bda68871eee9badfbf937f7a8e
Rashard Pleasant <rashard.pleasant@gmail.com>
2019-06-19 21:54:48 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,6 +2,8 @@ Katz_deli = []
  => ["The line is currently empty"]
 end
 
-take_a_number = ("number")
- puts ("Katz_deli"["name"])
- end
+take_a_number = ("name")
+ => ("Katz_deli"["name"])
+end
+
+ Now_serving = ()
