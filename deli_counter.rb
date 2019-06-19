273885cadd73ba0c257e1697b2fd84dc1bbf1621
Rashard Pleasant <rashard.pleasant@gmail.com>
2019-06-19 21:59:57 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,16 +1,18 @@
-
+require
 Katz_deli = []
  => ["The line is currently empty"]
  puts ("someone should be in the line")
+
 end
 
 take_a_number = ("name")
  => ("Katz_deli"["name"])
  puts ("Katz_deli ["Mike"])
  
-end
+ end
 
  Now_serving = ("number")
   puts ("There is nobody waiting to be served")
    => ("Now serving ["mike"])
+ 
  end
